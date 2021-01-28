1da2179011d25212e17b18b9dc45508a1a778ae7
lkn <812091690@qq.com>
2021-01-28 16:24:58 +0800

    新增json文件转换类

--- src/test/java/formatExcel.java
+++ src/test/java/formatExcel.java
@@ -1,3 +1,4 @@
+import com.nettyTest.Util.FormatToJson;
 import com.nettyTest.Util.ReadExcelTools;
 import org.apache.poi.openxml4j.opc.internal.ContentType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -8,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,6 +17,105 @@ import java.util.List;
  */
 public class formatExcel {
     public static void main(String[] args) {
+//        List<Integer> arr = new ArrayList<>();
+//        arr.add(1);
+//        arr.add(2);
+//        arr.add(3);
+//        arr.add(4);
+//        String s = FormatToJson.object2json(arr);
+//        System.out.println(s);
+
+        String filePath = "D:\\work\\元素站配置表.xlsx";
+        String outFilePath = "D:\\work\\json\\template.json";
+        try {
+            List<String[]> strings = ReadExcelTools.readExcel(filePath, 0);
+
+            //key
+            String[] keyStrings = strings.get(0);
+            //type
+            String[] typeStrings = strings.get(1);
+
+            int startIndex = 2;
+
+            StringBuffer buffer = new StringBuffer();
+            for (int k = startIndex, legt = strings.size(); k < legt; k++) {
+                if (k == 2) {
+                    buffer.append("[");
+                }
+                for (int i = 1, len = typeStrings.length; i < len; i++) {
+                    if (i == 1) {
+                        buffer.append("{");
+                    }
+                    String type = typeStrings[i];
+                    String value = strings.get(k)[i];
+                    String result = jsonTypeFormat(type, value);
+
+                    value = "\"" + keyStrings[i] + "\"" + ":" + result;
+                    buffer.append(value);
+
+                    if (i == (typeStrings.length - 1)) {
+                        if (k != (strings.size() - 1)) {
+                            buffer.append("}");
+                        } else if (k != (strings.size() - 1)) {
+                            buffer.append("},");
+                        }
+                    } else {
+                        buffer.append(",");
+                    }
+                }
+                if (k != (strings.size() - 1)) {
+                    buffer.append(",");
+                } else if (k == (strings.size() - 1)) {
+                    buffer.append("}]");
+                }
+            }
+
+
+            writeFile(buffer.toString(), outFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * 根据类型转换成不同的json值
+     *
+     * @param type
+     * @param value
+     */
+    private static String jsonTypeFormat(String type, String value) {
+        String resultValue = "";
+        if (value == null || value.equals("")) {
+            return resultValue;
+        }
+
+        switch (type) {
+            case "int":
+                resultValue = value;
+                break;
+            case "str":
+                resultValue = "\"" + value + "\"";
+                break;
+            case "arr":
+                resultValue = FormatToJson.array2json(value.split(","));
+                break;
+            case "list":
+                resultValue = FormatToJson.list2json(Arrays.asList(value.split(",")));
+            default:
+                System.out.println("不支持的数据类型!!");
+                break;
+        }
+
+        return resultValue;
+    }
+
+
+    /**
+     * 读取excel计算
+     */
+    private static void readExcelM() {
         String filePath = "D:\\work\\智能体升级速查表3-1-27.xlsx";
         String outFilePath = "D:\\work\\验算结果.txt";
         try {
@@ -50,8 +151,6 @@ public class formatExcel {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
 
@@ -82,6 +181,30 @@ public class formatExcel {
 
 
     /**
+     * 写入数据
+     *
+     * @param str         写入字符串
+     * @param outFilePath 写入文件路径
+     */
+    private static void writeFile(String str, String outFilePath) {
+        try {
+            File file = new File(outFilePath);
+            FileWriter fileWriter = new FileWriter(file);
+
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(new String(str.getBytes(), "utf-8"));
+
+
+            fileWriter.flush();
+            printWriter.flush();
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
      * 验算结果
      *
      * @param dw    段位
@@ -93,7 +216,7 @@ public class formatExcel {
      * @return
      */
     private static int checkCul(int dw, int lv, int RD, int rf, int speed, int hp) {
-        int cul = 0;
+        int cul;
 
         //研发能力*研发速度*保留系数1
         cul = RD * speed * 1;
